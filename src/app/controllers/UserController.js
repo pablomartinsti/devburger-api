0b666ed8f3199c1fbdd723a75505a03bbc074ad6
pablomartinsti <pablomartinsti@outlook.com>
2024-06-18 19:02:30 -0300

    commit

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,24 +1,40 @@
-import {v4} from 'uuid'
+import { v4 } from 'uuid'
+import * as Yup from 'yup'
 import User from '../models/User.js'
 
-class UserController{
-    async store (request,response) {
-        const {name, email, password_hash, admin} = request.body;
+class UserController {
+    async store(request, response) {
+
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean(),
+
+        })
+        try {
+            schema.validateSync(request.body,{ abortEarly:false} );
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
+
+        const { name, email, password_hash, admin } = request.body;
 
         const user = await User.create({
             id: v4(),
-            name ,
-            email ,
-            password_hash,                
+            name,
+            email,
+            password_hash,
             admin,
-           });
-        
-           return response.status(201).json({
+        });
+
+        return response.status(201).json({
             id: user.id,
             name,
             email,
             admin,
-           });
+        });
     }
 }
 export default new UserController();
