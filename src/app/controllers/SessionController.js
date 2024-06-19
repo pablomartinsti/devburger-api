d82d776f2f8e97d02330ca16f79c5deb8c402cd1
pablomartinsti <pablomartinsti@outlook.com>
2024-06-18 22:11:13 -0300

    commit

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,50 @@
+import * as Yup from 'yup';
+import User from '../models/User.js'
+
+class SessionController {
+    async store(request, response) {
+        const schema = Yup.object({
+            email: Yup.string().email().required(),
+            password: Yup.string().min(6).required()
+        });
+
+        const isValid = await schema.isValid(request.body);
+
+        const emailOrPasswordIncorrect = () =>{
+
+            return response.status(401).json({ erro: 'make sure email or password are correct' })
+
+        }
+
+        if (!isValid) {
+
+          return  emailOrPasswordIncorrect();
+           
+        }
+
+        const { email, password } = request.body;
+
+        const user = await User.findOne({
+            where: {
+                email,
+            }
+        });
+
+        if (!user) {
+            
+            return  emailOrPasswordIncorrect();
+        }
+
+        const isSamePassword = await user.checkPassword(password);
+
+        if (!isSamePassword) {
+            
+            return  emailOrPasswordIncorrect();
+        }
+
+        return response.status(201).json({ id: user.id, name: user.name, email, admin: user.admin })
+    }
+
+}
+
+export default new SessionController
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,30 +2,41 @@ import { v4 } from 'uuid'
 import * as Yup from 'yup'
 import User from '../models/User.js'
 
+
 class UserController {
     async store(request, response) {
 
         const schema = Yup.object({
             name: Yup.string().required(),
             email: Yup.string().email().required(),
-            password_hash: Yup.string().min(6).required(),
+            password: Yup.string().min(6).required(),
             admin: Yup.boolean(),
 
         })
         try {
-            schema.validateSync(request.body,{ abortEarly:false} );
+            schema.validateSync(request.body, { abortEarly: false });
         } catch (err) {
             return response.status(400).json({ error: err.errors })
         }
 
+        const { name, email, password, admin } = request.body;
 
-        const { name, email, password_hash, admin } = request.body;
+
+        const userExists = await User.findOne({
+            where: {
+                email,
+            },
+        });
+
+        if(userExists) {
+            return response.status(400).json({error: 'User already exists'})
+        }
 
         const user = await User.create({
             id: v4(),
             name,
             email,
-            password_hash,
+            password,
             admin,
         });
 
