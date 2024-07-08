ad2b6b8798dd0da5514654c1ee83590513bcd916
pablomartinsti <pablomartinsti@outlook.com>
2024-07-08 12:37:49 -0300

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express"
+import multer from "multer";
+import multerConfig from "./config/multer.js";
 import UserController from "./app/controllers/UserController.js";
 import SessionController from "./app/controllers/SessionController.js";
 import ProductController from "./app/controllers/ProductController.js";
@@ -6,7 +8,9 @@ import ProductController from "./app/controllers/ProductController.js";
 
 const routes = new Router()
 
+const upload = multer(multerConfig)
+
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
-routes.post('/products', ProductController.store)
+routes.post('/products',upload.single('file'), ProductController.store)
 export default routes;
