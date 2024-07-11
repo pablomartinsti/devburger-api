761ca1a3c772c2d5eec04d95df51550bc3f87e26
pablomartinsti <pablomartinsti@outlook.com>
2024-07-11 14:10:27 -0300

    commit

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,23 +1,79 @@
 import * as Yup from 'yup'
 import Order from '../schemas/Order.js';
+import Product from '../models/Product.js'
+import Category from '../models/Category.js';
+
+
 
 class OrderController {
 
-   async store(request, response) {
-   
+    async store(request, response) {
 
-        const order = {
-            user:{
-                id: request.userId,
-                name: request.userName
-            }
+        const schema = Yup.object({
+            products: Yup.array().required().of(
+                Yup.object({
+                    id: Yup.number().required(),
+                    quantity: Yup.number().required(),
+                }),
+            ),
+        })
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
         }
 
+
+        const { products } = request.body
+        const productsIds = products.map((product) => product.id)
+        const findProducts = await Product.findAll({
+            where: {
+                id: productsIds
+            },
+            include: [
+                {
+                    model: Category,
+                    as:'category',
+                    attributes: ['name'],
+                }
+            ]
+        })
+
+        const formattedProducts = findProducts.map(product =>{
+
+            const productIndex = products.findIndex((item) => item.id === product.id)
+            const newProduct = {
+                id: product.id,
+                name: product.name,
+                category: product.category.name,
+                price: product.price,
+                url: product.url,
+                quantity: products[productIndex].quantity
+            }
+            return newProduct
+        })
+
+
+        const order = {
+            user: {
+                id: request.userId,
+                name: request.userName
+            },
+
+            products: formattedProducts,
+            status: 'Pedido realizado',
+        }
+
+        const createOrder = await Order.create(order)
+
         
-        return response.status(201).json(order)
+
+
+        return response.status(201).json(createOrder)
     }
 
-  
+
 }
 
 
